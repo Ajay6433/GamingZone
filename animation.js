c214dbe579efb3ef015be68e9e084b90215cb792
Azay <81103330+Ajay6433@users.noreply.github.com>
2021-07-07 10:46:58 +0530

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,12 @@
+var typed = new Typed('.slogen', {
+    strings: [
+        'EAT', 
+        'SLEEP',
+        'GAME',
+        'REPEAT'
+    ],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true
+});
+
---
+++ main.js
@@ -0,0 +1,8 @@
+var navlinks = document.getElementById("links1");
+
+function showMenu(){
+    navlinks.style.right= "0";
+}
+function hideMenu(){
+    navlinks.style.right= "-200px";
+}
